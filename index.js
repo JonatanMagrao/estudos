b587b0cb1977547e534627e9277f63abb8de9de4
Jonatan_Magrão_Mac <jonatan.magrao92@gmail.com>
2021-08-28 18:27:42 -0300

    adicionado modulos de estudo em node

---
+++ index.js
@@ -0,0 +1,7 @@
+const f = require('./node_modules/funcoes')
+const lista = require('./node_modules/lista')
+const mat = require('./node_modules/matematica')
+
+f.mostrar(f.nome)
+lista.forEach(item=>console.log(item))
+mat.som(10,10)
