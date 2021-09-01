31fa120a21afffd41e4e834c2474375d431ecfc5
Jonatan_Magrão_Mac <jonatan.magrao92@gmail.com>
2021-08-31 23:20:32 -0300

    jo-ken-po python game

---
+++ game.py
@@ -0,0 +1,30 @@
+from random import randint
+
+#variáveis
+itens = ['Pedra','Papel','Tesoura']
+linha = '=='*10
+cpu =  randint(1,3)
+
+#a escolha de jogada
+print(f'\n{linha}')
+print('[ 1 ] Pedra\n[ 2 ] Papel\n[ 3 ] Tesoura')
+print(f'{linha}')
+
+jogador = int(input('Escolha sua jogada: '))
+
+#ero ao escolher um número fora dos permitidos
+if jogador != 1 and jogador != 2 and jogador!= 3:
+    print('{:x^30}'.format('Opção inválida. Tente novamente!'))
+
+#segue para a jogatina com a regra do jogo
+else: 
+    jogada = 'Cpu jogou {} e você jogou {}'.format(itens[cpu-1],itens[jogador-1])
+    if (jogador == cpu + 1) or (jogador == 1 and cpu == 3) :
+        print(f'\n{jogada}')
+        print('{:*^40}\n'.format('Parabéns, você ganhou!'))
+    elif jogador == cpu:
+        print(f'\n{jogada}')
+        print('{:=^40}\n'.format('Empate!'))
+    else:
+        print(f'\n{jogada}')
+        print('{:-^40}\n'.format('Você perdeu!'))
