57be124f16441a159133bf9146d795a50792cb5e
Jonatan_Magrão_Mac <jonatan.magrao92@gmail.com>
2021-08-31 23:53:12 -0300

    counter to game function added

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 from random import randint
+from time import sleep
 
 #variáveis
 itens = ['Pedra','Papel','Tesoura']
@@ -19,12 +20,21 @@ if jogador != 1 and jogador != 2 and jogador!= 3:
 #segue para a jogatina com a regra do jogo
 else: 
     jogada = 'Cpu jogou {} e você jogou {}'.format(itens[cpu-1],itens[jogador-1])
+
+    # jo-ken-po delay time to show
+    print('\n{:#^23}'.format('   JO   '))
+    sleep(1)
+    print('{:#^23}'.format('   KEN  '))
+    sleep(1)
+    print('{:#^23}'.format('   PO   '))
+    sleep(1)
+
     if (jogador == cpu + 1) or (jogador == 1 and cpu == 3) :
         print(f'\n{jogada}')
-        print('{:*^40}\n'.format('Parabéns, você ganhou!'))
+        print('{:*^40}\n'.format('  Parabéns, você ganhou!  '))
     elif jogador == cpu:
         print(f'\n{jogada}')
-        print('{:=^40}\n'.format('Empate!'))
+        print('{:=^40}\n'.format('  Empate!  '))
     else:
         print(f'\n{jogada}')
-        print('{:-^40}\n'.format('Você perdeu!'))
+        print('{:-^40}\n'.format('  Você perdeu!  '))
